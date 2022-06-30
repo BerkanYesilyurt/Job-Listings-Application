3722d2e948b778cfb4014de9e038f6a61e6b87e0
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-01 01:34:21 +0300

    model created

---
+++ app/Models/Listing.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+
+    public static function find($id){
+        $listings = self::all();
+        foreach($listings as $listing){
+            if($listing['id'] == $id){
+                return $listing;
+            }
+        }
+    }
+
+
+}
