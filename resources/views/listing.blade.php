f1db26d6f7da1f3b457e44c42514351f65723080
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-02 23:10:13 +0300

    sections

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,4 +1,8 @@
+@extends('layout')
+
+@section('content')
 <h2>{{$listing['title']}}</h2>
 <p>
     {{$listing['description']}}
 </p>
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,3 +1,6 @@
+@extends('layout')
+
+@section('content')
 <h1>{{$heading}}</h1>
 
 @unless(count($listings) == 0)
@@ -12,3 +15,4 @@
 @else
     <p>No listings found.</p>
 @endunless
+@endsection
