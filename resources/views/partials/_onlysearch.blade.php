a5d9467e31a5984d1b888c5743bf92b0a8f82b57
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-03 22:58:10 +0300

    search partials added

---
+++ resources/views/partials/_onlysearch.blade.php
@@ -0,0 +1,16 @@
+<div class="n-hero-two-form-cat">
+    <div class="n-saech-two-form">
+        <form method="get" action="">
+
+            <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="job-title" placeholder="Search Keyword">
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                <button class="btn n-btn-flat" type="submit"> <i class="fa fa-search"></i></button>
+            </div>
+        </form>
+    </div>
+</div>
+
---
+++ resources/views/partials/_search.blade.php
@@ -0,0 +1,32 @@
+
+<div class="clearfix"></div>
+<input type="hidden" id="is_dashboard_page" value=" "><div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper"><section class="n-hero-section-two"   style="background:  url(img/business-people.png) 0 0 no-repeat; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; background-position: center center; background-attachment:scroll;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
+                                <div class="n-hero-two-box">
+                                    <div class="n-hero-two-main-text">
+                                        <h1>Your Dream Jobs is Waiting</h1>
+                                        <p>Subtitle</p>
+                                    </div>
+                                    <div class="n-hero-two-form-cat">
+                                        <div class="n-saech-two-form">
+                                            <form  method="get" action="">
+
+                                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                                    <div class="form-group">
+                                                        <label><i class="ti-briefcase"></i> Search Job</label>
+                                                        <input type="text" class="form-control" name="job-title" placeholder="Search Keyword"/>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                                    <button class="btn n-btn-flat" type="submit"> <i class="fa fa-search"></i></button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section></div></div></div></div>
