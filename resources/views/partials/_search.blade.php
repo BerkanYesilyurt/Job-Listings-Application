1a909f90ac813d88959df7c468aa75622525b390
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-05 22:31:04 +0300

    partials form actions

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -11,12 +11,12 @@
                                     </div>
                                     <div class="n-hero-two-form-cat">
                                         <div class="n-saech-two-form">
-                                            <form  method="get" action="">
+                                            <form  method="get" action="/">
 
                                                 <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
                                                     <div class="form-group">
                                                         <label><i class="ti-briefcase"></i> Search Job</label>
-                                                        <input type="text" class="form-control" name="job-title" placeholder="Search Keyword"/>
+                                                        <input type="text" class="form-control" name="search" placeholder="Search Keyword"/>
                                                     </div>
                                                 </div>
                                                 <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
