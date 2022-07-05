3cdd083a4599ac963bbb93567aea86ae958a0e79
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-05 22:29:41 +0300

    listing controller updated

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         return view('listings', [
-            'listings' => Listing::all()
+            'listings' => Listing::filterIt($request)
         ]);
     }
 
