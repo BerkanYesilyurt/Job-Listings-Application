5d86fbb10ecccc527cb41a3c76f93bde56f00150
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-05 22:30:12 +0300

    filter func added

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,23 @@ class Listing extends Model
 {
     use HasFactory;
 
+    public static function filterIt($request){
+        $all = self::all();
+        if(isset($request['search'])){
+            return self::query()
+                ->where('title', 'LIKE', '%' . request('search') . '%')
+                ->orWhere('description', 'LIKE', '%' . request('search') . '%')
+                ->orWhere('tags', 'LIKE', '%' . request('search') . '%')
+                ->get();
+        }
+        elseif(isset($request['tag'])){
+            return self::query()->where('tags', 'LIKE', '%' . request('tag') . '%')->get();
+        }
+        else{
+            return $all;
+        }
+
+    }
 
     public static function find($id){
         $listings = self::all();
