7cebd14eae9b4207531c4c3926b5476fd042d1de
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-06 23:29:11 +0300

    controller form validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -19,6 +20,26 @@ class ListingController extends Controller
                 'listing' => $listing
             ]);
 
+    }
+
+    public function create(){
+        return view('create');
+    }
+
+    public function store(Request $request){
+    $fields =  $request->validate([
+        'title' => 'required',
+        'company' => ['required', Rule::unique('listings', 'company')],
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email', Rule::unique('listings', 'email')],
+        'tags' => 'required',
+        'description' => 'required'
+    ]);
+
+    Listing::create($fields);
+
+    return redirect('/')->with('message', 'Listing created successfully.');
     }
 
 
