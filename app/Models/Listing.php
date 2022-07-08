57be19cef5f60c2ed44db5b5c982cb067c1289d2
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-08 23:22:08 +0300

    Update Listing.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,7 +9,7 @@ class Listing extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'company', 'location', 'website', 'email', 'description', 'tags'];
+    protected $fillable = ['title', 'logo', 'company', 'location', 'website', 'email', 'description', 'tags'];
 
     public static function filterIt($request){
         $all = self::all();
