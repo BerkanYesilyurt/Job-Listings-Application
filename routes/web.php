d2710e1229be423839518555eb72851ffc23a6b1
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-08 23:14:28 +0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', [ListingController::class, 'index']);
 Route::get('create', [ListingController::class, 'create']);
 Route::post('create', [ListingController::class, 'store']);
 Route::get('listings/{listing}', [ListingController::class, 'show']);
+Route::get('listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::put('listings/{listing}', [ListingController::class, 'update']);
+Route::delete('listings/{listing}', [ListingController::class, 'destroy']);
