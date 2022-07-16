7a219063cd2546e07e9289651b60ababff53a6e0
Berkan Yeşilyurt <berkan38212@gmail.com>
2022-07-16 23:56:58 +0300

    DatabaseSeeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,18 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
        Listing::factory(20)->create();
+        
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Admin Account',
+            'email' => 'test@test.com',
+            'email_verified_at' => null,
+            'password' => '$2y$10$9Wa0zXBUYPaptDysRlfqiehVRR.y2ZZ.P6PugH3JNL7.xcXQ9eacO',
+            'remember_token' => null,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
 
     }
 }
