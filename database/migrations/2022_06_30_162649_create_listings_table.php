cf4037ab5a99cea1efdb18b392141e1a60e8b3ff
Berkan Yeşilyurt <berkan38212@gmail.com>
2022-07-16 23:55:58 +0300

    added user_id for ownership

--- database/migrations/2022_06_30_162649_create_listings_table.php
+++ database/migrations/2022_06_30_162649_create_listings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('title');
             $table->string('logo')->nullable();
             $table->string('tags');
